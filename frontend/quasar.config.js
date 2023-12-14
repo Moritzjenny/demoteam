2fcdb397c46f045f4b3087ff993968bd4a785f8b
Moritzjenny <moritzjenny@hotmail.com>
2023-12-14 21:33:49 +0100

    new base

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -68,7 +68,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/demoteam/frontend',
+      publicPath: '/demoteam/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
